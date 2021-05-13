88fed87fc90f4440ec6ea5ae07a6599e442307cc
NeevesLab <75540337+NeevesLab@users.noreply.github.com>
2021-05-13 13:30:41 -0600

    Update GA_runner.py

--- GA_runner.py
+++ GA_runner.py
@@ -1,12 +1,14 @@
 import Genetic_Algorithm as GA
 import HCA_Multiple_Countercurrent as HCA
 import os
-
+# options to pass into genetic algorhtm
 options=dict(popSize=100, eliteSize=10, mutationRate=0.05,
              runtime='target_error',generations=500,target_error=0.03)
+# solve for device with a given set of constrains using the genetic algorhtm
 lengths,df=GA.geneticAlgorithm(**options)
+# compute hydraulic circuit analysis for device design on the output from the GA
 circiut = HCA.HCA_from_spacing(lengths)
-
+# store results in csvs
 filename='Countercurrent Design'
 i=0
 while os.path.exists(filename+str(i)+'.csv'):
